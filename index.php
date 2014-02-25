3062ff0344294ba637812a1a059c50a0d4ecfafd
lauraprem <premillieu.laura@gmail.com>
2014-02-25 17:27:32 +0100

    Gestion des erreurs && Factorisation de la connexion a la bdd

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 require 'Modele.php';
 
-$billets = getBillets();
-
-require 'vueAccueil.php';
+try {
+  $billets = getBillets();
+  require 'vueAccueil.php';
+}
+catch (Exception $e) {
+  echo '<html><body>Erreur ! ' . $e->getMessage() . '</body></html>';
+}
